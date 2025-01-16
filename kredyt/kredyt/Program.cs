9c7e201b87ca7b675d751b4a62dd8cce0b5e1eaa
Bartosz Syldatk <bartosz-syldatk@outlook.com>
2025-01-16 20:04:15 +0100

    Add files via upload

---
+++ kredyt/kredyt/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO.Pipelines;
+
+Console.WriteLine("Kwota kredytu:");
+int kwota = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Oprocentowanie w skali roku:");
+double procent = Convert.ToDouble(Console.ReadLine());
+procent = procent / 100;
+
+Console.WriteLine("Ilość rat:");
+int raty = Convert.ToInt16(Console.ReadLine());
+
+double stopa = procent / 12;
+
+double R = (kwota * stopa * Math.Pow((1 + stopa), raty)) / (Math.Pow((1 + stopa), raty) - 1);
+
+Console.WriteLine("Miesięczna rata kredytu: {0:0.00}", R);
